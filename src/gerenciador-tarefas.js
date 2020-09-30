69d2e44f300fbb5409dc260b60a7cd99acf92ebb
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-09-30 17:01:50 -0300

    feat: definindo as rotas para os componentes

--- src/gerenciador-tarefas.js
+++ src/gerenciador-tarefas.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './gerenciador-tarefas.css';
 import { useRoutes } from 'hookrouter';
+import ListarTarefas from './listar/listar-tarefas';
+import CadastrarTarefa from './cadastrar/cadastrar-tarefa';
+import AtulaizarTarefa from './atualizar/atualizar-tarefa';
 
 const routes = {
   '/': () => <ListarTarefas />,
