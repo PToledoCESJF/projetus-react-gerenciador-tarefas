5db2fa1f86f18b1f85dd05797bfbf7ff7234ac1e
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-09-30 18:31:47 -0300

    feat: implementando a funçao de cadastro de tarefas

--- src/cadastrar/cadastrar-tarefa.js
+++ src/cadastrar/cadastrar-tarefa.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Form, Jumbotron, Modal } from 'react-bootstrap';
 import { navigate, A } from 'hookrouter';
+import Tarefa from '../models/tarefa-model';
 
 function CadastrarTarefa() {
 
@@ -9,7 +10,18 @@ function CadastrarTarefa() {
     const [exibirModal, setExibirModal] = useState(false);
 
     function cadastrar(event){
+        event.preventDefault();
+        setFormValidado(true);
+        if(event.currentTarget.checkValidity() === true){
+            // obtém as tarefas
+            const tarefasDb = localStorage['tarefas'];
+            const tarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
 
+            // persiste a tarefa
+            tarefas.push(new Tarefa(new Date().getTime, tarefa, false));
+            localStorage['tarefas'] = JSON.stringify(tarefas);
+            setExibirModal(true);
+        }
     }
 
     function handleTxtTarefa(event){
@@ -18,6 +30,8 @@ function CadastrarTarefa() {
 
     function handleFecharModal(){
         navigate('/');
+        setExibirModal(false);
+
     }
 
     return (
@@ -42,7 +56,7 @@ function CadastrarTarefa() {
                         <A href="/" className="btn btn-light">Voltar</A>
                     </Form.Group>
                 </Form>
-                <Modal show={false} onHide={handleFecharModal}>
+                <Modal show={exibirModal} onHide={handleFecharModal}>
                     <Modal.Header closeButton><Modal.Title>Sucesso</Modal.Title></Modal.Header>
                     <Modal.Body>Tarefa adicionada com sucesso</Modal.Body>
                     <Modal.Footer><Button variant="success" onClick={handleFecharModal}>Continuar</Button></Modal.Footer>
