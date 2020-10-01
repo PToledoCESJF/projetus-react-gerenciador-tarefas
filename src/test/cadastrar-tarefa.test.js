b36c19524618ac02f9a6f810dc2e055925f61df2
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-10-01 17:13:47 -0300

    test: criando os testes do cadastro de tarefas

--- src/test/cadastrar-tarefa.test.js
+++ src/test/cadastrar-tarefa.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import CadastrarTarefa from '../cadastrar/cadastrar-tarefa';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Teste do componente de cadastro de tarefa', () => {
     test('Deve renderizar o componente corretamente', () => {
         const { getByText } = render(<CadastrarTarefa />);
         const linkElement = getByText(/Cadastrar Tarefa/i);
         expect(linkElement).toBeInTheDocument();
+    });
+
+    test('Deve cadastrar uma nova tarefa', () => {
+        const { getByTestId } = render(<CadastrarTarefa />);
+        fireEvent.change(getByTestId('txt-tarefa'), { target: { value: 'Testar componente' }});
+        fireEvent.click(getByTestId('btn-cadastrar'));
+        expect(getByTestId('modal')).toHaveTextContent('Sucesso');
+        expect(getByTestId('modal')).toHaveTextContent('Tarefa adicionada com sucesso!')
     });
 });
