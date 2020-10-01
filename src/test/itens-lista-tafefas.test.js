6bbe81ce11808de2cef7fa1a0c02e57ccbbb92b3
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-10-01 20:09:15 -0300

    test: criando os testes do componente de itens das tarefas

---
+++ src/test/itens-lista-tafefas.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ItensListaTarefas from '../listar/itens-lista-tarefas';
+import Tarefa from '../models/tarefa-model';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Teste do componente que exibe um item da listagem de tarefas', () => {
+
+    const nomeTarefa = 'Tarefa';
+    const tarefa = new Tarefa(1, nomeTarefa, false);
+    const tarefaConcluida = new Tarefa(2, nomeTarefa, true);
+
+    test('Deve renderizar o componente sem erros', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ItensListaTarefas tarefas={[]} recarregarTarefas={() => false} />, div);
+        ReactDOM.unmountComponentAtNode(div);
+        
+    });
+
+    test('Deve exibir a tarefa', () => {
+        const { getByTestId } = render(
+            <table>
+                <tbody>
+                    <ItensListaTarefas tarefas={[tarefa]} recarregarTarefas={() => false} />
+                </tbody>
+            </table>
+        )
+       expect(getByTestId('tarefa')).toHaveTextContent(nomeTarefa);
+    });
+
+    test('Deve exibir uma tarefa concluida', () => {
+        const { getByTestId } = render(
+            <table>
+                <tbody>
+                    <ItensListaTarefas tarefas={[tarefaConcluida]} recarregarTarefas={() => false} />
+                </tbody>
+            </table>
+        )
+       expect(getByTestId('nome-tarefa')).toHaveStyle('text-decoration: line-through');
+    });
+});
